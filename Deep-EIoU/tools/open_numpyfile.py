758499275efde9a3644ccb4c31537388aedbf7c3
Zabu <sd23303@toyota-ti.ac.jp>
2025-02-13 14:30:21 +0900

    Clear cached files

--- Deep-EIoU/tools/open_numpyfile.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-data = np.load('/home/zabu/Deep-EIoU/Deep-EIoU/YOLOX_outputs/yolox_x_ch_sportsmot/emb_gt/v_i2_L4qquVg0_c010.npy', allow_pickle=True)
-print(data[:5])  # Inspect the first 5 elements
